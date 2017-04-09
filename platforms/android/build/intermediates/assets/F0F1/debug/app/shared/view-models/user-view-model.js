dbc5b533897f7ebbcc38d0351c7b176ec08b528c
Harsha Vamsi <harshavamsi096@gmail.com>
2017-04-09 09:31:01 +0530

    Added firebase auth

---
+++ platforms/android/build/intermediates/assets/F0F1/debug/app/shared/view-models/user-view-model.js
@@ -0,0 +1,66 @@
+var config = require("../../shared/config");
+var fetchModule = require("fetch");
+var Observable = require("data/observable").Observable;
+var validator = require("email-validator");
+
+function User(info) {
+    info = info || {};
+
+    // You can add properties to observables on creation
+    var viewModel = new Observable({
+        email: info.email || "",
+        password: info.password || ""
+    });
+
+    viewModel.login = function() {
+        return fetchModule.fetch(config.apiUrl + "oauth/token", {
+            method: "POST",
+            body: JSON.stringify({
+                username: viewModel.get("email"),
+                password: viewModel.get("password"),
+                grant_type: "password"
+            }),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+        .then(handleErrors)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            config.token = data.Result.access_token;
+        });
+    };
+
+    viewModel.register = function() {
+        return fetchModule.fetch(config.apiUrl + "Users", {
+            method: "POST",
+            body: JSON.stringify({
+                Username: viewModel.get("email"),
+                Email: viewModel.get("email"),
+                Password: viewModel.get("password")
+            }),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        }).then(handleErrors);
+    };
+
+    viewModel.isValidEmail = function() {
+        var email = this.get("email");
+        return validator.validate(email);
+    };
+
+    return viewModel;
+}
+
+function handleErrors(response) {
+    if (!response.ok) {
+        console.log(JSON.stringify(response));
+        throw Error(response.statusText);
+    }
+    return response;
+}
+
+module.exports = User;
